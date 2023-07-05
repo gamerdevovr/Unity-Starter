575f5c525db1b70820acfa0c7809b6feb0548e3d
gamerdev <gamerdevovr@outlook.com>
2023-07-05 16:18:34 +0300

    Lesson 6 - Input System.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,20 +3,55 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 _moveMent = new Vector3(1, 0, 0);
-
-    [SerializeField]
     private Vector3 _position = new Vector3(-10, 0.17f, 0);
+    [SerializeField]
+    private float _jumpForce;
+    [SerializeField]
+    private float _speedWalk;
 
-    private void Start()
+    private PlayerControls _conrols;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
     {
+        _conrols = new PlayerControls();
+        _conrols.Player.Jump.performed += _ => Jump();
+
+        _rigidbody = GetComponent<Rigidbody>();
+
         transform.position = _position;
     }
 
+    private void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        _conrols.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _conrols.Player.Disable();
+    }
+
+    private void Update()
+    {
+        
+    }    
+
     private void FixedUpdate()
     {
-        transform.position = transform.position + _moveMent * Time.fixedDeltaTime;
+        Vector2 moveDirection_v2 = _conrols.Player.Move.ReadValue<Vector2>();
+        Vector3 moveDirection_v3 = new Vector3(moveDirection_v2.x, 0, moveDirection_v2.y);
 
-        //transform.LookAt(new Vector3(0, 0, 0));
+        _rigidbody.AddForce(moveDirection_v3 * _speedWalk);
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(new Vector3(0, _jumpForce, 0), ForceMode.Impulse);
     }
 }
